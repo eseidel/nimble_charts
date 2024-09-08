0808696772197ca7dfb5d19365a26021f20bc32a
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-09-08 18:35:56 +1000

    fix legend layout problem

--- charts_flutter/lib/src/behaviors/legend/legend_layout.dart
+++ charts_flutter/lib/src/behaviors/legend/legend_layout.dart
@@ -126,23 +126,19 @@ class TabularLegendLayout implements LegendLayout {
   }
 
   Widget _buildVerticalFirst(List<Widget> legendEntries) {
-    //TODO: Fix
-    throw UnimplementedError(
-      'This code is trying to append an unmodifiable list in TableRow',
-    );
+    final maxRows = (desiredMaxRows == _noLimit)
+        ? legendEntries.length
+        : min(legendEntries.length, desiredMaxRows);
 
-    // final maxRows = (desiredMaxRows == _noLimit)
-    //     ? legendEntries.length
-    //     : min(legendEntries.length, desiredMaxRows);
+    final rows =
+        //Don't use const here, as the TableRow children need to be mutable
+        // ignore: avoid_redundant_argument_values, prefer_const_constructors, prefer_const_literals_to_create_immutables
+        List.generate(maxRows, (_) => TableRow(children: <Widget>[]));
+    for (var i = 0; i < legendEntries.length; i++) {
+      rows[i % maxRows].children.add(legendEntries[i]);
+    }
 
-    // final rows = List.generate(maxRows, (_) => const TableRow());
-
-    // for (var i = 0; i < legendEntries.length; i++) {
-    //   children here is unmodifiable. How did this work in the first place?
-    //   rows[i % maxRows].children.add(legendEntries[i]);
-    // }
-
-    // return _buildTableFromRows(rows);
+    return _buildTableFromRows(rows);
   }
 
   Table _buildTableFromRows(List<TableRow> rows) {
