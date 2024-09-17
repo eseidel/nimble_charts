5f57d3cdf6ee77fb400ee94d29c41c0567aa0459
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-09-18 09:19:47 +1000

    refactor

--- charts_flutter/test/example_widget_tests/example_test.dart
+++ charts_flutter/test/example_widget_tests/example_test.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: avoid_relative_lib_imports
 
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nimble_charts/flutter.dart' as charts;
 
@@ -11,15 +12,35 @@ void main() {
   group('ExampleApp Widget Tests', () {
     m.useRandomData = false;
 
-    testWidgets('Navigates to BarChart and verifies rendering', (tester) async {
-      await tester.pumpWidget(m.GalleryApp());
-      expect(find.byType(h.Home), findsOneWidget);
-      await tester.tap(find.text('Simple Bar Chart'));
-      await tester.pumpAndSettle();
-      expect(find.byType(charts.BarChart), findsOneWidget);
-      await tester.tap(find.byType(charts.BarChart));
-      await tester.pumpAndSettle();
-      await matchesGolden<charts.BarChart>('example_simple_bar_chart');
+    testWidgets('Navigates to Simple Bar Chartand Renders', (tester) async {
+      const buttonText = 'Simple Bar Chart';
+
+      await tester.navigateToChartAndGolden(
+        buttonText,
+        extra: () async {
+          await tester.tap(find.byType(charts.BarChart));
+          await tester.pumpAndSettle();
+        },
+      );
     });
   });
+}
+
+extension ExampleWidgetTestExtensions on WidgetTester {
+
+  /// Taps on the button and takes a golden screenshot of the chart.
+  Future<void> navigateToChartAndGolden(
+    String buttonText, {
+    Future<void> Function()? extra,
+  }) async {
+    await pumpWidget(m.GalleryApp());
+    expect(find.byType(h.Home), findsOneWidget);
+    await tap(find.text(buttonText));
+    await pumpAndSettle();
+    expect(find.byType(charts.BarChart), findsOneWidget);
+    await extra?.call();
+    await matchesGolden<charts.BarChart>(
+      'example_${buttonText.replaceAll(' ', '_').toLowerCase()}',
+    );
+  }
 }
