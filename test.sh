2d89f49a76b14e9372d7c0489bfde027b859e33d
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-09-19 07:33:26 +1000

    bump

--- test.sh
+++ test.sh
@@ -9,3 +9,4 @@ flutter test --update-goldens --coverage
 lcov  ./coverage --output-file ./coverage/lcov.info --capture --directory
 
 genhtml ./coverage/lcov.info --output-directory ./coverage/html 
+
