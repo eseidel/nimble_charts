62b2c9dfb58a534b112ffc62a88944bcd53af23d
Christian Findlay <16697547+MelbourneDeveloper@users.noreply.github.com>
2024-09-24 07:53:44 +1000

    doco

--- charts_flutter/lib/src/bar_chart.dart
+++ charts_flutter/lib/src/bar_chart.dart
@@ -13,61 +13,83 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'dart:collection' show LinkedHashMap;
-
 import 'package:nimble_charts/flutter.dart';
 import 'package:nimble_charts/src/base_chart_state.dart';
 import 'package:nimble_charts/src/cartesian_chart.dart' as cart;
 import 'package:nimble_charts_common/common.dart' as common;
 
+/// A bar chart that displays one or more series of data as a set of rectangles.
+///
+/// Each rectangle represents a single data point in the series and has a length
+/// proportional to the value of that data point.
 class BarChart extends cart.CartesianChart<String> {
-  final bool vertical;
-  final common.BarRendererDecorator<String>? barRendererDecorator;
-
-  // ignore: sort_constructors_first, use_super_parameters
+  /// Creates a bar chart with the given series of data.
+  ///
+  /// [seriesList] The list of series to be plotted on the chart.
+  /// [animate] Whether to animate the chart when data changes.
+  /// [animationDuration] The duration of the animation when data changes.
+  /// [domainAxis] The domain axis to use for the chart. If not provided, a
+  /// default axis will be used.
+  /// [primaryMeasureAxis] The primary measure axis to use for the chart. If
+  /// not provided, a default numeric axis will be used.
+  /// [secondaryMeasureAxis] The secondary measure axis to use for the chart,
+  /// if needed.
+  /// [disjointMeasureAxes] A map of measure axes for disjoint domains,
+  /// if needed.
+  /// [barGroupingType] The type of grouping to use for multiple series.
+  /// [defaultRenderer] The default renderer to use for the series. If not
+  /// provided, a default BarRendererConfig will be used.
+  /// [customSeriesRenderers] Custom renderers for specific series, if needed.
+  /// [behaviors] A list of chart behaviors to attach to the chart.
+  /// [selectionModels] A list of selection models to attach to the chart.
+  /// [rtlSpec] Configures the chart for right-to-left languages.
+  /// [vertical] Whether to render the bars vertically (true) or horizontally
+  /// (false).
+  /// [defaultInteractions] Whether to add the default interactions to the
+  /// chart.
+  /// [layoutConfig] Configuration for chart layout.
+  /// [userManagedState] Allows external management of chart state.
+  /// [barRendererDecorator] A decorator for the bar renderer.
+  /// [flipVerticalAxis] Whether to flip the vertical axis of the chart.
   BarChart(
-    List<common.Series<dynamic, String>> seriesList, {
+    super.seriesList, {
     super.key,
-    bool? animate,
-    Duration? animationDuration,
-    common.AxisSpec? domainAxis,
-    common.NumericAxisSpec? primaryMeasureAxis,
-    common.NumericAxisSpec? secondaryMeasureAxis,
-    LinkedHashMap<String, common.NumericAxisSpec>? disjointMeasureAxes,
+    super.animate,
+    super.animationDuration,
+    super.domainAxis,
+    super.primaryMeasureAxis,
+    super.secondaryMeasureAxis,
+    super.disjointMeasureAxes,
     common.BarGroupingType? barGroupingType,
     common.BarRendererConfig<String>? defaultRenderer,
-    List<common.SeriesRendererConfig<String>>? customSeriesRenderers,
-    List<ChartBehavior<String>>? behaviors,
-    List<SelectionModelConfig<String>>? selectionModels,
-    common.RTLSpec? rtlSpec,
+    super.customSeriesRenderers,
+    super.behaviors,
+    super.selectionModels,
+    super.rtlSpec,
     this.vertical = true,
-    bool defaultInteractions = true,
-    LayoutConfig? layoutConfig,
-    UserManagedState<String>? userManagedState,
+    super.defaultInteractions,
+    super.layoutConfig,
+    super.userManagedState,
     this.barRendererDecorator,
-    bool? flipVerticalAxis,
+    super.flipVerticalAxis,
   }) : super(
-          seriesList,
-          animate: animate,
-          animationDuration: animationDuration,
-          domainAxis: domainAxis,
-          primaryMeasureAxis: primaryMeasureAxis,
-          secondaryMeasureAxis: secondaryMeasureAxis,
-          disjointMeasureAxes: disjointMeasureAxes,
           defaultRenderer: defaultRenderer ??
               common.BarRendererConfig<String>(
                 groupingType: barGroupingType,
                 barRendererDecorator: barRendererDecorator,
               ),
-          customSeriesRenderers: customSeriesRenderers,
-          behaviors: behaviors,
-          selectionModels: selectionModels,
-          rtlSpec: rtlSpec,
-          defaultInteractions: defaultInteractions,
-          layoutConfig: layoutConfig,
-          userManagedState: userManagedState,
-          flipVerticalAxis: flipVerticalAxis,
         );
+
+  /// Whether the bars are rendered vertically or horizontally.
+  ///
+  /// If true, the bars will be rendered vertically with the domain axis along
+  /// the bottom of the chart. If false, the bars will be rendered horizontally
+  /// with the domain axis along the left side of the chart.
+  final bool vertical;
+
+  /// A decorator for the bar renderer that can add extra drawing on top of the
+  /// bars.
+  final common.BarRendererDecorator<String>? barRendererDecorator;
 
   @override
   common.BaseChart<String> createCommonChart(
